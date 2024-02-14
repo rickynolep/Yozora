e60c0eb1e0cbb79417b6b26ae6eaa7a6d752e177
rickynolep <rickynolepcuy@gmail.com>
2024-02-14 10:23:10 +0800

    Fixing commit

--- commands/utility/intro.js
+++ commands/utility/intro.js
@@ -11,61 +11,52 @@ function getColorHex(colorName) {
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('intro')
-		.setDescription('Manage introductions')
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('create')
-				.setDescription('Membuat sebuah perkenalan yang sudah diembed dan dikirim secara otomatis')
-				.addStringOption(option =>
-					option.setName('name')
-						.setDescription('Siapakah Namamu?')
-						.setRequired(true))
-				.addStringOption(option =>
-					option.setName('description')
-						.setDescription('ada kata kata perkenalan? contoh: salken')
-						.setRequired(true))
-				.addStringOption(option =>
-					option.setName('age')
-						.setDescription('Berapa umurmu?')
-						.setRequired(false))
-				.addStringOption(option =>
-					option.setName('hobby')
-						.setDescription('Apa Hobimu?')
-						.setRequired(false))
-				.addStringOption(option =>
-					option.setName('color')
-						.setDescription('Mengganti warna garis embed menggunakan kode hex')
-						.setRequired(false))
-				.addStringOption(option =>
-					option.setName('location')
-						.setDescription('Dari mana kamu?')
-						.setRequired(false))),
+		.setDescription('Membuat sebuah perkenalan yang sudah diembed dan dikirim secara otomatis')
+		.addStringOption(option =>
+			option.setName('name')
+				.setDescription('Siapakah Namamu?')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('description')
+				.setDescription('ada kata kata perkenalan? contoh: salken')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('age')
+				.setDescription('Berapa umurmu?')
+				.setRequired(false))
+		.addStringOption(option =>
+			option.setName('hobby')
+				.setDescription('Apa Hobimu?')
+				.setRequired(false))
+		.addStringOption(option =>
+			option.setName('color')
+				.setDescription('Mengganti warna garis embed menggunakan kode hex')
+				.setRequired(false))
+		.addStringOption(option =>
+			option.setName('location')
+				.setDescription('Dari mana kamu?')
+				.setRequired(false)),
 	async execute(interaction) {
-		const subcommand = interaction.options.getSubcommand();
-		if (!interaction.isCommand() || interaction.commandName !== 'intro') return;
 		const user = interaction.user;
 		const thumbnailURL = user.avatarURL({ dynamic: true, size: 512 }) || 'https://example.com/default-thumbnail.jpg';
-
-		if (subcommand === 'create') {
-			const name = interaction.options.getString('name');
-			const age = interaction.options.getString('age') || '-';
-			const hobby = interaction.options.getString('hobby') || '-';
-			const location = interaction.options.getString('location') || '-';
-			const description = interaction.options.getString('description');
-			const colorInput = interaction.options.getString('color') || '#0099ff';
-			const color = getColorHex(colorInput);
-			if (!color) {
-				await interaction.reply({ content: 'Warna tidak valid. Pastikan Anda menggunakan [kode hex](https://www.google.com/search?q=Pemilih+warna) yang valid.', ephemeral: true });
-				return;
-			}
-
-			const introEmbed = new EmbedBuilder()
-				.setColor(color)
-				.setTitle('Perkenalkan')
-				.setThumbnail(thumbnailURL)
-				.setDescription(`Nama: ${name}\nUmur: ${age}\nHobi: ${hobby}\nDaerah asal: ${location}\n\n${description}`);
-
-			await interaction.reply({ embeds: [introEmbed] });
+		const name = interaction.options.getString('name');
+		const age = interaction.options.getString('age') || '-';
+		const hobby = interaction.options.getString('hobby') || '-';
+		const location = interaction.options.getString('location') || '-';
+		const description = interaction.options.getString('description');
+		const colorInput = interaction.options.getString('color') || '#0099ff';
+		const color = getColorHex(colorInput);
+		if (!color) {
+			await interaction.reply({ content: 'Warna tidak valid. Pastikan Anda menggunakan [kode hex](https://www.google.com/search?q=Pemilih+warna) yang valid.', ephemeral: true });
+			return;
 		}
+
+		const introEmbed = new EmbedBuilder()
+			.setColor(color)
+			.setTitle('Perkenalkan')
+			.setThumbnail(thumbnailURL)
+			.setDescription(`Nama: ${name}\nUmur: ${age}\nHobi: ${hobby}\nDaerah asal: ${location}\n\n${description}`);
+
+		await interaction.reply({ embeds: [introEmbed] });
 	},
 };
